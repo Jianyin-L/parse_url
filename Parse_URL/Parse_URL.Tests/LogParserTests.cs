80a8f7efd6180c9560c50fada92f5cc5738f77f0
Jenny L <lijianyin05@gmail.com>
2025-02-10 18:00:08 +1100

    add more tests

--- Parse_URL/Parse_URL.Tests/LogParserTests.cs
+++ Parse_URL/Parse_URL.Tests/LogParserTests.cs
@@ -154,82 +154,22 @@ public class LogParserTests
         Assert.Empty(result);
     }
 
-    //[Fact]
-    //public void ParseLogFile_ShouldCountUniqueIPs()
-    //{
-    //    var parser = new LogParser();
-    //    string logFilePath = "test_log.txt";
-
-    //    File.WriteAllLines(logFilePath, new[]
-    //    {
-    //    "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /home HTTP/1.1\" 200 3574 \"-\" \"UserAgent1\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:29 +0200] \"GET /about HTTP/1.1\" 200 3574 \"-\" \"UserAgent2\"",
-    //    "177.71.128.21 - - [10/Jul/2018:22:21:30 +0200] \"GET /home HTTP/1.1\" 200 3574 \"-\" \"UserAgent3\""
-    //    });
-
-    //    var result = parser.ParseLogFile(logFilePath);
-
-    //    Assert.Equal(2, result.UniqueIPs);
-    //}
-
-    //[Fact]
-    //public void ParseLogFile_ShouldReturnTopUrls()
-    //{
-    //    var parser = new LogParser();
-    //    string logFilePath = "test_log.txt";
-
-    //    File.WriteAllLines(logFilePath, new[]
-    //    {
-    //    "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /home HTTP/1.1\" 200 3574 \"-\" \"UserAgent1\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:29 +0200] \"GET /about HTTP/1.1\" 200 3574 \"-\" \"UserAgent2\"",
-    //    "177.71.128.21 - - [10/Jul/2018:22:21:30 +0200] \"GET /home HTTP/1.1\" 200 3574 \"-\" \"UserAgent3\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:31 +0200] \"GET /contact HTTP/1.1\" 200 3574 \"-\" \"UserAgent4\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:32 +0200] \"GET /about HTTP/1.1\" 200 3574 \"-\" \"UserAgent5\""
-    //    });
-
-    //    var result = parser.ParseLogFile(logFilePath);
-
-    //    Assert.Equal(3, result.TopUrls.Count);
-    //    Assert.Equal("/home", result.TopUrls.Keys.First());
-    //}
-
-    //[Fact]
-    //public void ParseLogFile_ShouldReturnTopIPs()
-    //{
-    //    var parser = new LogParser();
-    //    string logFilePath = "test_log.txt";
-
-    //    File.WriteAllLines(logFilePath, new[]
-    //    {
-    //    "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /home HTTP/1.1\" 200 3574 \"-\" \"UserAgent1\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:29 +0200] \"GET /about HTTP/1.1\" 200 3574 \"-\" \"UserAgent2\"",
-    //    "177.71.128.21 - - [10/Jul/2018:22:21:30 +0200] \"GET /home HTTP/1.1\" 200 3574 \"-\" \"UserAgent3\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:31 +0200] \"GET /contact HTTP/1.1\" 200 3574 \"-\" \"UserAgent4\"",
-    //    "168.41.191.40 - - [10/Jul/2018:22:21:32 +0200] \"GET /about HTTP/1.1\" 200 3574 \"-\" \"UserAgent5\""
-    //});
-
-    //    var result = parser.ParseLogFile(logFilePath);
-
-    //    Assert.Equal(2, result.TopIPs.Count);
-    //    Assert.Equal("168.41.191.40", result.TopIPs.Keys.First());
-    //}
-
     [Fact]
-    public void CountUniqueItems_ShouldReturnUniqueIPs()
+    public void CountUniqueItems_ShouldReturnNumberOfUniqueIPs()
     {
         var logEntries = new List<LogEntry>
         {
-            new() { IPAddress = "123.12.123.12"},
-            new() { IPAddress = "123.12.123.12"},
-            new() { IPAddress = "222.12.123.12"}
+            new() { IPAddress = "111.11.111.11"},
+            new() { IPAddress = "111.11.111.11"},
+            new() { IPAddress = "222.22.222.22"}
         };
 
-        int result = LogParser.CountUniqueItems(logEntries, log => log.IPAddress);
+        var result = LogParser.CountUniqueItems(logEntries, log => log.IPAddress);
         Assert.Equal(2, result);
     }
 
     [Fact]
-    public void GetTopItems_ShouldReturnTopItems()
+    public void GetTopItems_ShouldReturnTopURLs()
     {
         var logEntries = new List<LogEntry>
         {
@@ -240,7 +180,7 @@ public class LogParserTests
             new() { Url = "/contact"}
         };
 
-        var result = LogParser.GetTopItems(logEntries, log => log.Url, 2);  //TODO: include cases where n > logEntries.Count
+        var result = LogParser.GetTopItems(logEntries, log => log.Url, 2);
 
         Assert.Equal(2, result.Count);
         Assert.Equal(2, result["/home"]);
@@ -248,7 +188,82 @@ public class LogParserTests
     }
 
     [Fact]
-    public void GetTopItems_ShouldReturnTopItemsIncludingTies()
+    public void GetTopItems_ShouldReturnTopIPs()
+    {
+        var logEntries = new List<LogEntry>
+        {
+            new() { IPAddress = "111.11.111.11"},
+            new() { IPAddress = "111.11.111.11"},
+            new() { IPAddress = "222.22.222.22"},
+            new() { IPAddress = "222.22.222.22"},
+            new() { IPAddress = "222.22.222.22"},
+            new() { IPAddress = "333.33.333.33"}
+        };
+
+        var result = LogParser.GetTopItems(logEntries, log => log.IPAddress, 2);
+
+        Assert.Equal(2, result.Count);
+        Assert.Equal(2, result["111.11.111.11"]);
+        Assert.Equal(3, result["222.22.222.22"]);
+    }
+
+    [Fact]
+    public void CountUniqueItems_ShouldReturnNumberOfUniqueItems()
+    {
+        var logEntries = new List<LogEntry>
+        {
+            new() { Url = "/home"},
+            new() { Url = "/home"},
+            new() { Url = "/about"},
+            new() { Url = "/about"},
+            new() { Url = "/contact"}
+        };
+
+        var result = LogParser.CountUniqueItems(logEntries, log => log.Url);
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void GetTopItems_ShouldReturnTopNItems()
+    {
+        var logEntries = new List<LogEntry>
+        {
+            new() { Url = "/home"},
+            new() { Url = "/home"},
+            new() { Url = "/about"},
+            new() { Url = "/about"},
+            new() { Url = "/contact"}
+        };
+
+        var result = LogParser.GetTopItems(logEntries, log => log.Url, 2);
+
+        Assert.Equal(2, result.Count);
+        Assert.Equal(2, result["/home"]);
+        Assert.Equal(2, result["/about"]);
+    }
+
+    [Fact]
+    public void GetTopItems_ShouldReturnTopItemsWhereNHigherThanNoOfEntries()
+    {
+        var logEntries = new List<LogEntry>
+        {
+            new() { Url = "/home"},
+            new() { Url = "/home"},
+            new() { Url = "/about"},
+            new() { Url = "/about"},
+            new() { Url = "/contact"}
+        };
+
+        var result = LogParser.GetTopItems(logEntries, log => log.Url, 4);
+
+        Assert.Equal(3, result.Count);
+        Assert.Equal(2, result["/home"]);
+        Assert.Equal(2, result["/about"]);
+        Assert.Equal(1, result["/contact"]);
+    }
+
+    [Fact]
+    public void GetTopItems_ShouldReturnTopNItemsIncludingTies()
     {
         var logEntries = new List<LogEntry>
         {
@@ -264,6 +279,8 @@ public class LogParserTests
         Assert.Equal(2, result["/home"]);
         Assert.Equal(2, result["/about"]);
     }
+
+    //handle empty fields
 
     private string CreateTestLogFile(params string[] logEntries)
     {
